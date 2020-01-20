6a7f67d4362d2030a830b122e609f8f546a18c75
Tyler Oalman <tyleroalman@gmail.com>
2020-01-20 01:11:33 -0600

    Implement `print!`/`println!` macros

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,7 @@ mod vga_buffer;
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(
-        vga_buffer::WRITER.lock(),
-        ", some numbers: {} {}",
-        42,
-        1.337
-    )
-    .unwrap();
+    println!("Hello World{}", "!");
 
     loop {}
 }
