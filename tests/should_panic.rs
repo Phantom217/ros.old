9292ddb8af1d1e6bdcbb272454fb3c3a12750be0
Tyler Oalman <tyleroalman@gmail.com>
2020-01-20 22:28:59 -0600

    Disable the test harness for the `should_panic` test

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,8 +1,5 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
@@ -10,24 +7,13 @@ use ros::{exit_qemu, serial_print, serial_println, QemuExitCode};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    test_main();
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failed);
 
     loop {}
 }
 
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
 fn should_fail() {
     serial_print!("should_fail... ");
     assert_eq!(0, 1);
