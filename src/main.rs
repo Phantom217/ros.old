ea4d13508759b9c7d77c7bb673b6514471acd43b
Tyler Oalman <tyleroalman@gmail.com>
2020-01-22 18:26:25 -0600

    Add a double fault handler and test it

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,10 @@ pub extern "C" fn _start() -> ! {
 
     ros::init();
 
-    // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3();
+    // trigger a page fault
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    };
 
     #[cfg(test)]
     test_main();
