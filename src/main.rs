73f8f96b8d7b4d7f5b8626838498dc3895507a70
Tyler Oalman <tyleroalman@gmail.com>
2020-01-24 17:57:23 -0600

    Remove deadlock provoking code

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,7 @@ pub extern "C" fn _start() -> ! {
     test_main();
 
     println!("It did not crash!");
-    loop {
-        use ros::print;
-        print!("-");
-    }
+    loop {}
 }
 
 /// This function is called on panic.
